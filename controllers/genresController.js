05b1c3c59473e509418de96b93f1ff455425e445
apacz1 <112419536+apacz1@users.noreply.github.com>
2024-10-25 18:40:38 +0200

    Added edit and add pages, updated controllers and routes, styled.

--- controllers/genresController.js
+++ controllers/genresController.js
@@ -1,18 +1,23 @@
 const db = require("../db/queries");
 
 async function getGames(req, res) {
-  console.log(req.url);
+  console.log(req.params);
   const genreName = req.params.genre_name.toLowerCase();
   console.log(genreName);
   if (genreName !== "all") {
     const genreResult = await db.getGenreByName(genreName);
     const genreId = genreResult[0].genre_id;
     const games = await db.getGamesByGenre(genreId);
-    res.render("genres", { games: games });
+    res.render("genres", { games: games, params: req.params });
   } else if (genreName === "all") {
     const games = await db.getAllGames();
-    res.render("genres", { games: games });
+    res.render("genres", { games: games, params: req.params });
   }
 }
 
-module.exports = { getGames };
+async function getGenres(req, res) {
+  const genres = await db.getAllGenres();
+  res.render("add", { genres: genres, params: req.params });
+}
+
+module.exports = { getGames, getGenres };
--- routes/genresRouter.js
+++ routes/genresRouter.js
@@ -3,5 +3,7 @@ const genresController = require("../controllers/genresController");
 const genresRouter = Router();
 
 genresRouter.get("/:genre_name", genresController.getGames);
+genresRouter.get("/:genre_name/add", genresController.getGenres);
+genresRouter.get("/:genre_name/edit");
 
 module.exports = genresRouter;
